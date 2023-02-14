fe7530e70330fe793afa03f9fe44d7eb222c79e1
hussein <husseinyasser2077@gmail.com>
2023-02-15 01:16:37 +0200

    Init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-import 'package:note_app/constant/constant_routes.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constant/constant.dart';
 import 'package:note_app/routes.dart';
 
 import 'package:note_app/view/screens/note_view.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
